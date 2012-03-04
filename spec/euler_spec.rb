ac836ed552c1fc69e22a649b202a95587393d33e
Paul Groudas <me@paulgroudas.com>
2012-03-03 22:37:49 -0500

    Various misc. improvements to project layout

--- spec/euler_spec.rb
+++ spec/euler_spec.rb
@@ -1,15 +1,21 @@
 require 'euler'
 
 describe Euler do
-  it 'The sum of all natural numbers below 1000 that are multiples of 3 or 5 should be "233168"', :problem_1 => true do
+
+  it 'Problem 1: The sum of all natural numbers below 1000 that are multiples of 3 or 5 should be "233168"', :problem_1 => true do
     Euler.problem_1.should == 233168;
   end
 
-  it 'The sum of the even values in the fibonacci sequence below 4 million should be "4613732"', :problem_2 => true do
+  it 'Problem 2: The sum of the even values in the fibonacci sequence below 4 million should be "4613732"', :problem_2 => true do
     Euler.problem_2.should == 4613732
   end
 
-  it 'The largest prime factor of "600851475143" should be "6857"', :problem_3 => true do
+  it 'Problem 3: The largest prime factor of "600851475143" should be "6857"', :problem_3 => true do
     Euler.problem_3.should == 6857
   end
+
+  it 'Problem 4: The largest palindromic product of 2 3-digit numbers should be "906609"', :problem_4 => true do
+    Euler.problem_4.should == 906609
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,17 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'fibonacci'
-require 'utils'
+
+# Require all files in lib directory
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each do |file| 
+  require File.basename(file, File.extname(file))
+end
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each do |file|
+  require File.basename(file, File.extname(file))
+end
 
 RSpec.configure do |config|
 end
